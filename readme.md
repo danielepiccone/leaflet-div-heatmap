69e105b3919b73f37b4f5688c6ed726533805929
mildtaste <mild.taste@gmail.com>
2013-10-13 20:30:06 +0200

    update

--- readme.md
+++ readme.md
@@ -2,16 +2,37 @@
 
 Lightweight implementation of an heatmap layer for leaflet using CSS3 and divIcon
 
+##Use
+
+```
+var heatmapLayer = new L.HeatmapLayer();
+```
+
+###Options
+
+```
+{
+    clickable: true or false, // heatmapLayer.on('click',function() {...});
+    radius: 100,
+    gradient: true or false, // Make solid blobs
+    color: '255,0,0' or '#FF0000'
+}
+```
 
 ##Methods:
 
 ###.setData(data)
 Loads an array of data points:
 
+```
 var data = [
 {
     lat:10,
     lon:12,
     value:1
+},
+{
+...
 }
 ];
+```
--- src/leaflet-heatmap.js
+++ src/leaflet-heatmap.js
@@ -7,8 +7,10 @@
 
 L.HeatmapLayer = L.FeatureGroup.extend({
     options: {
-        color: '200,20,20',
-        radius: 100
+        color: '#00ee44',
+        radius: 100,
+        gradient: true,
+        clickable: false
     },
 
     initialize: function(options){
@@ -18,16 +20,17 @@ L.HeatmapLayer = L.FeatureGroup.extend({
         // Define CSS rule for making heatmap invisible to clicking
         var style = document.createElement('style');
         document.head.appendChild(style);
-        // Apparently some version of Safari needs the following line? I dunno.
-        style.appendChild(document.createTextNode(''));
-        var sheet = style.sheet;
-        sheet.insertRule(".leaflet-heatmap-blob { pointer-events:none }", 0);
+
+        // Make the heatmap invisible to clicking 
+        //style.appendChild(document.createTextNode(''));
+        //var sheet = style.sheet;
+        //sheet.insertRule(".leaflet-heatmap-blob { pointer-events:none }", 0);
     },
 
     _parseColor: function (color) {
         m = color.match(/^#([0-9a-f]{6})$/i);
         if(m) {
-            return (parseInt(m.substr(0,2),16) + ',' + parseInt(m.substr(2,2),16)+ ',' + parseInt(m.substr(4,2),16));
+            return (parseInt(m[1].substr(0,2),16) + ',' + parseInt(m[1].substr(2,2),16)+ ',' + parseInt(m[1].substr(4,2),16));
         }else{
             return color;
         }
@@ -37,24 +40,35 @@ L.HeatmapLayer = L.FeatureGroup.extend({
         if (!value || !lat || !lng) {
             throw new Error('Provide a latitude, longitude and a value');
         }
-        
-        //value = value > 1 ? 1 : value;
+
+        if (value > 1 || value < 0) {
+            throw new Error('Value should be beetween 0 and 1');
+        }
+
+        // Define the marker
+        var alpha_start = value,
+            alpha_end = !this.options.gradient ? value : 0,
+            opacity = value;
+
+
+        var gradient =  'radial-gradient(closest-side, rgba('+ this._parseColor(this.options.color) +', '+alpha_start+') 0%, rgba('+ this._parseColor(this.options.color) +', '+alpha_end+') 100%)';
+        var html = '<div class="heatblob" data-value="'+value+'" style="width:100%;height:100%;border-radius:50%;background-image:'+gradient+'">';
         var size = this.options.radius * value;
-        //value = 1;
+        
         L.marker([lat, lng], {
             icon: L.divIcon({
                 iconSize: [ size, size ],
                 className: 'leaflet-heatmap-blob',
-                html: '<div class="heatblob" data-value="'+value+'" style="width:100%;height:100%;border-radius:50%;background-image: radial-gradient(closest-side, rgba('+ this._parseColor(this.options.color) +', '+value+') 0%, rgba('+ this._parseColor(this.options.color) +', 0) 100%)">'
+                html: html
             }),
-            clickable: false,
+            clickable: this.options.clickable,
             keyboard: false,
-            opacity: value
+            opacity: opacity
         }).addTo(this);
     },
 
     setData: function(data) {
-        // Data object is three values [{lat,lon,value}]
+        // Data object is three values [ {lat,lon,value}, {...}, ...]
         this.clearLayers();
         var self = this;
         data.forEach(function(point){
@@ -64,7 +78,7 @@ L.HeatmapLayer = L.FeatureGroup.extend({
     },
 
     /*
-    * Debug
+    * Testing
     */
 
     addTestPoints: function(number) {
